33041d27a139451b5ce509957439efc728f61956
Andrei Yarmukhametov <andyarus@gmail.com>
2019-10-13 00:57:21 +0500

    Contacts tableView added

--- ContactList/View Controllers/ContactsViewController.swift
+++ ContactList/View Controllers/ContactsViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class ContactsViewController: UIViewController {
+  
+  // MARK: - Properties
+  let tableView = UITableView()
 
+  // MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    setup()
+    load()
+  }
+  
+  // MARK: - Setup Methods
+  func setup() {
+    setupTableView()
+  }
+  
+  func setupTableView() {
+    view.addSubview(tableView)
+    
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      tableView.topAnchor.constraint(equalTo: view.topAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+      ])
+  }
+  
+  func load() {
+    
   }
 
 }
