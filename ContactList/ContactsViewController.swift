67a2f51befd63a9c7f60291612e54a5e047745f1
Andrei Yarmukhametov <andyarus@gmail.com>
2019-10-12 22:29:29 +0500

    Init

---
+++ ContactList/ContactsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContactsViewController.swift
+//  ContactList
+//
+//  Created by Andrei Coder on 12/10/2019.
+//  Copyright © 2019 yaav. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
