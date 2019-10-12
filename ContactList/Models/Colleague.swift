977b4031d55566e8aafc165d0c78ca637e889e22
Andrei Yarmukhametov <andyarus@gmail.com>
2019-10-13 00:03:22 +0500

    Models added

---
+++ ContactList/Models/Colleague.swift
@@ -0,0 +1,20 @@
+//
+//  Colleague.swift
+//  ContactList
+//
+//  Created by Andrei Coder on 12/10/2019.
+//  Copyright © 2019 yaav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colleague {
+  let firstName: String
+  let lastName: String
+  let middleName: String?
+  let photo: UIImage
+  let phone: String  
+  let workPhone: String
+  let positin: String
+}
---
+++ ContactList/Models/Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  ContactList
+//
+//  Created by Andrei Coder on 12/10/2019.
+//  Copyright © 2019 yaav. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Friend {
+  let firstName: String
+  let lastName: String
+  let middleName: String?
+  let photo: UIImage
+  let phone: String  
+  let birthday: Date
+}
